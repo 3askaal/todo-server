fddb79c7b0bc38cf6839d506e79204b12ae3721a
Bas Kaal <3askaal@gmail.com>
2020-04-09 11:52:45 +0200

    add types to resolvers

--- server/resolvers/TodoResolvers.ts
+++ server/resolvers/TodoResolvers.ts
@@ -1,36 +1,36 @@
 import { Col } from '../models'
-import { TTodo } from '../types/todo.d'
+import { TMutationResolvers, TQueryResolvers } from '../types/todo.d'
 
-export const Query = {
+export const Query: TQueryResolvers = {
   Todos: async () => {
     const todos = await Col.find({})
     return todos
   },
 }
 
-export const Mutation = {
-  createTodo: async (root: any, { data }: any): Promise<TTodo> => {
+export const Mutation: TMutationResolvers = {
+  createTodo: async (_, { data }) => {
     const todo = await Col.create(data)
     return todo
   },
-  updateTodo: async (root: any, { _id, data }: any): Promise<TTodo> => {
+  updateTodo: async (_, { _id, data }) => {
     const todo = await Col.findByIdAndUpdate(_id, data, { new: true })
     return todo
   },
-  deleteTodo: async (root: any, { _id }: any): Promise<TTodo> => {
+  deleteTodo: async (_, { _id }) => {
     const todo = await Col.findByIdAndRemove(_id)
     return todo
   },
-  checkAll: async (): Promise<boolean> => {
+  checkAll: async () => {
     const res = await Col.updateMany({}, { completed: true })
     return res
   },
-  uncheckAll: async (): Promise<boolean> => {
+  uncheckAll: async () => {
     const res = await Col.updateMany({}, { completed: false })
     return res
   },
-  deleteAll: async (): Promise<any> => {
+  deleteAll: async () => {
     const res = await Col.remove({})
-    return res
+    return !!res
   },
 }
