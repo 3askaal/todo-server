9ec6ea4204fe7ef0cd56c795ad14184e5a436ec1
Bas Kaal <3askaal@gmail.com>
2020-05-08 13:18:51 +0200

    progress

--- server/data/todos.ts
+++ server/data/todos.ts
@@ -1,9 +1,9 @@
 export const TODOS_DATA: any = {
   data: [
-    { content: 'write todo app server (NodeJS/Typescript)', completed: true },
-    { content: 'write tests for todo app server', completed: true },
-    { content: 'write todo app client (React/Typescript)', completed: true },
-    { content: 'write tests for todo app client', completed: true },
+    { content: 'write todo-app server (NodeJS/Typescript/GraphQL)', completed: true },
+    { content: 'write tests for todo-app server', completed: true },
+    { content: 'write todo-app client (React/Typescript/Apollo)', completed: true },
+    { content: 'write tests for todo-app client', completed: true },
     { content: 'walk the dogs at my parents house' },
     { content: 'find awesome new job' },
   ],
